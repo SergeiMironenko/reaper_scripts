88338781a6bbc577b01f7dff995b6d083f2d5800
SergeiMironenko <90122255+SergeiMironenko@users.noreply.github.com>
2023-05-12 13:02:27 +0700

    breaking bad

--- SetTimeCode.lua
+++ SetTimeCode.lua
@@ -1,19 +1,32 @@
-mediaItems = reaper.CountMediaItems(0)
+-- mediaItems = reaper.CountMediaItems(0)
 
-for i = 0, mediaItems - 1, 1 do
-    item = reaper.GetMediaItem(0, i)
-    take = reaper.GetTake(item, 0)
-    
-    proj = 0
-    isrgn = true
-    pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
-    rgnend = pos + reaper.GetMediaItemInfo_Value(item, 'D_LENGTH')
+-- for i = 0, mediaItems - 1, 1 do
+--     item = reaper.GetMediaItem(0, i)
+--     take = reaper.GetTake(item, 0)
 
-    nameFull = reaper.GetTakeName(take)
-    pos1 = string.find(nameFull, ".", 1, true) - 1
-    -- reaper.ShowConsoleMsg(pos1)
-    -- pos2 = string.find(nameFull, ".", pos1, true) - 1
-    name = "CHAP=" .. string.sub(nameFull, 1, pos1)
-    wantidx = i
-    reaper.AddProjectMarker(proj, isrgn, pos, rgnend, name, wantidx)
+--     proj = 0
+--     isrgn = true
+--     pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
+--     rgnend = pos + reaper.GetMediaItemInfo_Value(item, 'D_LENGTH')
+
+--     nameFull = reaper.GetTakeName(take)
+--     pos1 = string.find(nameFull, ".", 1, true) - 1
+--     -- reaper.ShowConsoleMsg(pos1)
+--     -- pos2 = string.find(nameFull, ".", pos1, true) - 1
+--     name = "CHAP=" .. string.sub(nameFull, 1, pos1)
+--     wantidx = i
+--     reaper.AddProjectMarker(proj, isrgn, pos, rgnend, name, wantidx)
+-- end
+
+buf = reaper.GetProjectPath()
+-- file = io.open(buf .. "/tracks", "r")
+path = string.format([[dir "%s" /b]], buf)
+for dir in io.popen(path):lines() do
+    subpath = string.format([[dir "%s\%s" /b]], buf, dir)
+    for subdir in io.popen(subpath):lines() do
+        -- reaper.ShowConsoleMsg(subdir)
+    end
 end
+-- reaper.ShowConsoleMsg(path)
+
+reaper.ShowConsoleMsg(reaper.GetNumTracks())
